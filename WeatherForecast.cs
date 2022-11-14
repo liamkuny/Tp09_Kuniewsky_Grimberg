41e22a5803a2b84c58bbed91196637bf8ac964c3
LIAM KUNIEWSKY <46584168@ort>
2022-11-14 08:21:12 -0300

    new webapi

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Tp09_Kuniewsky_Grimberg;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
